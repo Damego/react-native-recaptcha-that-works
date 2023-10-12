37601b9596ddd894499aa098e882b32aa3d31e5f
Damego <damego.dev@gmail.com>
2023-10-12 19:44:48 +0500

    chore: remove extra styles for invisible size

--- src/Recaptcha.js
+++ src/Recaptcha.js
@@ -41,10 +41,6 @@ const styles = StyleSheet.create({
   },
   invisibleView: {
     position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
   },
   loadingContainer: {
     position: 'absolute',
